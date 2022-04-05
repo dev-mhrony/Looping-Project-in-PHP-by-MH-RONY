287bb7b9634ada70f9eb99081d9ec592efe697f4
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-05 23:16:05 +0600

    Last update = 05/04/2022

--- Assignment 05/index.php
+++ Assignment 05/index.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment 05 - </title>
+    <title>Assignment 05 - Foreach </title>
 </head>
 <body>
     <?php
 
+    // Product Name 
+    $product_list = ['apple', 'orange', 'banana', 'mango'] ;
 
+    
+
+    foreach ($product_list as $key => $name){
+        echo ++$key . ' '. $name ;                  // Since the default value of the key starts from 0, its value is added to 1 and a specified in front of it.
+        echo "<br>" ;
+    }
  
-
+        // ============================================ Thank You ==================================================
 
 
     ?>
