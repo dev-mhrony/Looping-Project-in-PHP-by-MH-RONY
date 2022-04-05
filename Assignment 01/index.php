6a4561300d80bc72901e064c0fd6629e814a6598
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-05 23:16:59 +0600

    last update - 05/04/2022

--- Assignment 01/index.php
+++ Assignment 01/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo cal_days_in_month(CAL_GREGORIAN, 4, 2022);
+   
+
+    
+        $corrent_month = cal_days_in_month(CAL_GREGORIAN, 4, 2022) ;
+
+        
+
+    
+            for ($corrent_month = 1 ; $corrent_month <= 30 ; $corrent_month++){
+           
+                echo $corrent_month ;
+                echo "<br>" ;
+            }
+            
+    
