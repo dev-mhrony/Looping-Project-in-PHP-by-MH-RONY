f271a77a5c52640232cb45f9b108d71735129b4b
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-05 18:30:47 +0600

    Last update = 05/04/2022

---
+++ Assignment 04/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 04 -while loop (decrement operator) </title>
+</head>
+<body>
+    <?php
+
+
+        // Print out from 100 - 1 using a while loop. 
+
+        // You must use decrement operator
+
+
+    $input = 100 ;       // Here you have to input the maximum value or the value of a.
+
+    while ($input >= 1){
+        echo "This Number is: $input" ;
+        echo "<br>";        
+        $input--;
+    }
+
+
+
+?>
+</body>
+</html>
