2ba3b830262df5c4fb66b8db5921dd7a73e62109
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-06 14:41:41 +0600

    Factorial Formula Update - 06/04/2022

--- Assignment 03 (Factorial Formula)/index.php
+++ Assignment 03 (Factorial Formula)/index.php
@@ -5,16 +5,16 @@
 
 
 
-    $number = 8;                                        // Your Chose Factorial Number .
+    // $number = 8;                                        // Your Chose Factorial Number .
     
-    $factorial = 1;                                     //The n(n-1) formula is used to determine each factorial value. For which the value of factorial has been taken as "1".
+    // $factorial = 1;                                     //The n(n-1) formula is used to determine each factorial value. For which the value of factorial has been taken as "1".
    
-    for ($valu = $number; $valu>=1 ; $valu--){          // There are two reasons for giving $ a = input_number. (1. To shorten the name. //// 2. Here it has been done for the initialization )
+    // for ($valu = $number; $valu>=1 ; $valu--){          // There are two reasons for giving $ a = input_number. (1. To shorten the name. //// 2. Here it has been done for the initialization )
     
-        $factorial = $factorial * $valu;
+    //     $factorial = $factorial * $valu;
    
-        echo " This $number Factorial is $factorial <br>" ;
-    }
+    //     echo " This $number Factorial is $factorial <br>" ;
+    // }
 
 
 
