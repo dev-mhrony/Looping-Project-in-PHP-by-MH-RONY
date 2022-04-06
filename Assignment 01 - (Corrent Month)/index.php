e027789ad40e5c9e7bd3108c6f554cdad7dd4610
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-06 14:55:12 +0600

    last update

--- Assignment 01 - (Corrent Month)/index.php
+++ Assignment 01 - (Corrent Month)/index.php
@@ -1,6 +1,16 @@
 <?php
 
 
+
+        // Question 01:
+
+        // Your client said that he needs a dropdown, which contains all days of the running month. Sometimes we fill up a registration form that, which date of birth. So find out in google (get number of days in specific month php) and do this assignment using a for loop.
+
+        // Day quantity is not equal for every month. Like as - february - 28, March - 31, april - 30
+
+        // So days of running month should dynamic (by php build in function)
+
+
     //Your Corrent Month Hear 
     $place_your_month = 2;
 
