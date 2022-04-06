cd7f853b68c77e8974381c2435d5564b8ac5be6c
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-06 14:39:46 +0600

    Last Update - 06/04/2022

--- Assignment 03 (Factorial Formula)/index.php
+++ Assignment 03 (Factorial Formula)/index.php
@@ -1,7 +1,35 @@
 <?php
 
 
-    for ($a = 7 ; $a >=1 ; $a--){
-        echo $a *=$a;
-        echo "<br>" ;
+    // ========================================================= Case 01 ===============================================
+
+
+
+    $number = 8;                                        // Your Chose Factorial Number .
+    
+    $factorial = 1;                                     //The n(n-1) formula is used to determine each factorial value. For which the value of factorial has been taken as "1".
+   
+    for ($valu = $number; $valu>=1 ; $valu--){          // There are two reasons for giving $ a = input_number. (1. To shorten the name. //// 2. Here it has been done for the initialization )
+    
+        $factorial = $factorial * $valu;
+   
+        echo " This $number Factorial is $factorial <br>" ;
     }
+
+
+
+    // ========================================================= Case 02 ===============================================
+    
+    
+    
+    $number = 8;                                        // Your Chose Factorial Number .
+    
+    $factorial = 1;                                     //The n(n-1) formula is used to determine each factorial value. For which the value of factorial has been taken as "1".
+    
+    for ($valu = $number; $valu>=1 ; $valu--){          // There are two reasons for giving $ a = input_number. (1. To shorten the name. //// 2. Here it has been done for the initialization )
+    
+        $factorial = $factorial * $valu;
+        
+    }echo " This $number Factorial is $factorial" ;
+
+?>
